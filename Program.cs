09edec72350ac037247846f68637ed1b000e8ff3
Abdul-Rahman Abdurrohman <abdulbarakeh98@gmail.com>
2021-03-10 23:55:33 +0100

    Functional sorting

--- Program.cs
+++ Program.cs
@@ -7,28 +7,49 @@ namespace Quicksort
 {
     class Program
     {
-        List<int> orderedLesser = new List<int>();
-        List<int> orderedGreater = new List<int>();
         static void Main(string[] args)
         {
-            int[] unsortedArray = {21,547,8,56,6,357,32,487,3,4,547,243,4,1,48,5,46,78,1877,7,54,8,5,46,23,1,567,61,54,61,3};
+            var unsortedArray = new int[] {21,547,8,56,6,357,32,487,3,4,547,243,4,1,48,5,46,78,1877,7,54,8,5,46,23,1,567,61,54,61,3};
 
-        }
-        private int DivideArray(int[] array, int first, int last){
-            int pivot = array[last];
-            
-            return pivot;
-        }
-        private void Sort(int[] array, int first , int last){
-            int[] sortedArray = array;
-            for (int i = 0; i < array.Length; i++)
+            var sortedArray = Sort(unsortedArray, 0, unsortedArray.Length);
+
+            foreach (var item in sortedArray)
             {
-                if (array[i]<=pivot)
-                {
-                    
-                }
+                Console.WriteLine(item);
             }
-            return sortedArray;
+        }
+        private static int[] Sort(int[] array, int first, int last){
+            if (first < last)
+            {
+                var pivot = Partition(array, first, last);
+                Sort(array,first,pivot);
+                Sort(array,pivot+1,last);
+            }
+
+            return array;
+        }
+        private static void Swap(int[] array, int first, int last)
+        {
+            var temp = array[first];
+            array[first] = array[last];
+            array[last] = temp;
+        }
+        private static int Partition(int[] array, int first , int last){
+            int pivot = array[first];
+            int swapIndex = first;
+
+            for (int testIndex = first+1; testIndex < last; testIndex++)
+            {
+                if (array[testIndex]<pivot)
+                {
+                    swapIndex++;
+                    Swap(array, testIndex, swapIndex);
+                }
+
+            }
+            Swap(array, first, swapIndex);
+
+            return swapIndex;
         }
 
 
